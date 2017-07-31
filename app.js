a4dac17cd5288000439c4e5db7f288bc9a074b6a
An Le <lean@Ans-MacBook-Air.local>
2017-07-31 18:17:01 -0400

    nunjucks

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
 
 app.use(function (req, res, next) {
-  console.log(req.method, res.statusCode);
+  morgan(':id :method :url :response-time');
   next();
 })
 
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
 app.get('/', function(req,res,next){
-  res.send('Welcome!');
+  const names = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  res.render( 'index', {title: 'Hall of Fame', people: names} );
 })
 app.get('/news', function(req,res,next){
   res.send('Welcome to the news page!');
