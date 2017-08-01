8ff55dbe53aba85e3b7f90097ce5ce0d22812d37
Sarah Denaci <sarahdenaci@gmail.com>
2017-08-01 10:25:51 -0400

    beginning of router and stylesheet

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
+const routes = require('./routes');
+
+app.use('/', routes);
 
 app.use(function (req, res, next) {
   morgan(':id :method :url :response-time');
@@ -11,16 +14,7 @@ app.use(function (req, res, next) {
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
-app.get('/', function(req,res,next){
-  const names = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-  res.render( 'index', {title: 'Hall of Fame', people: names} );
-})
-app.get('/news', function(req,res,next){
-  res.send('Welcome to the news page!');
-})
-app.get('/special/', function(req,res,next){
-  res.send('you reached the special area!');
-})
+
 app.listen(3000, function(){
   console.log('listening to port 3000');
 })
